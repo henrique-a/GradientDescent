7d501279a7fd9764e9a03a3137adbf858aab5144
henrique-a <henriquearaujoandrade@gmail.com>
2019-09-17 20:37:52 +0200

    newton's method not working yet

--- gradientDescendent.py
+++ gradientDescendent.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def f(x1, x2):
-	(1 - x1)^2 + 100*(x2 - x1)^2
+	(1 - x1)**2 + 100*(x2 - x1)**2
 
 def df(x1, x2):
 	return np.array([-2*(1-x1) - 200*(x2 - x1), 200*(x2 - x1)])
@@ -11,10 +11,9 @@ def gd(x_0, e):
 	x_c = x_0
 	i = 0
 	while 1:
-		delta = -df(x_c[0], x_c[1])
-		x_new = x_c + 0.001*delta
+		delta = -1 * df(x_c[0], x_c[1])
+		x_new = x_c + 0.001 * delta
 		x_c = x_new
-		#print(np.linalg.norm(delta))
 		i = i + 1
 		print("Iteration: {}".format(i))		
 		print(x_new)
--- gradientDescendentNewton.py
+++ gradientDescendentNewton.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 def f(x1, x2):
 	(1 - x1)^2 + 100*(x2 - x1)^2
@@ -6,30 +7,33 @@ def f(x1, x2):
 def df(x1, x2):
 	return np.array([-2*(1-x1) - 200*(x2 - x1), 200*(x2 - x1)])
 
-def ddf():
-	return np.array([[202, -200], [-200, 200]])
+
+def g(theta, delta, x):
+	x = x + theta * delta
+	return f(x[0], x[1])
 
 def dg(theta, delta, x):
-	return df(x[0] + theta * delta, x[1] + theta * delta)
+	return -2 * delta[0] * (1 - x[0] - theta * delta[0]) + 100 * (delta[1] - delta[0])
 
-def ddg():
-	return ddf()
+def ddg(delta):
+	return 2 * delta[0]**2
 
 def gd(x_0, e):
 	x_c = x_0
 	i = 0
-	
-	while 1:
-		theta_c = 0
-		delta = -df(x_c[0], x_c[1])
-		x_new = x_c + theta_c*delta
-		x_c = x_new
-		j = 0
-		while j < 10: 
-			j = j + 1
-			theta_new = theta_c - np.linalg.inv(ddg()) @ dg(theta_c, delta, x_c)
+	while i < 10:
+		i = i + 1
+		delta = -1 * df(x_c[0], x_c[1])
+		theta_c = 1
+		while abs(dg(theta_c, delta, x_c)) > 0.1:
+			theta_new = theta_c - dg(theta_c, delta, x_c) / ddg(delta)
 			theta_c = theta_new
-			
+			print(dg(theta_c, delta, x_c))
+
+		print("theta: {}".format(theta_c))
+		
+		x_new = x_c + theta_c * delta
+		x_c = x_new
 		print("Iteration: {}".format(i))		
 		print(x_new)
 		if np.linalg.norm(delta) < 0.01:
