5c10a185f2c69978ad2ce0cb1f86b88c723938a8
Santos De Andrade Henrique <henrique.santos_de_andrade@insa-rouen.fr>
2019-09-25 15:44:20 +0200

    Newton's method working

--- gradientDescendentNewton.py
+++ gradientDescendentNewton.py
@@ -21,24 +21,23 @@ def ddg(delta):
 def gd(x_0, e):
 	x_c = x_0
 	i = 0
+
 	while 1:
 		i = i + 1
 		delta = -1 * df(x_c[0], x_c[1])
-		theta_c = 1
-		while abs(dg(theta_c, delta, x_c)) > 0.1:
+		theta_c = 0
+		while abs(dg(theta_c, delta, x_c)) > 0.001 or theta_c == 0:
 			theta_new = theta_c - dg(theta_c, delta, x_c) / ddg(delta)
 			theta_c = theta_new
-			print(dg(theta_c, delta, x_c))
 
-		
-		
+		plt.scatter(x_c[0], x_c[1])
 		x_new = x_c + theta_c * delta
 		x_c = x_new
-		plt.scatter(x_new[0], x_new[1])
+		
 		print("Iteration: {}".format(i))
 		print("theta: {}".format(theta_c))		
 		print(x_new)
-		if np.linalg.norm(delta) < 0.01:
+		if np.linalg.norm(delta) < 0.0001:
 			plt.show()
 			return x_c
 
